aaf36936414638b1e5437880df5cef168b52b4dd
Prithvi <prinz.deagle@gmail.com>
2018-08-08 15:46:59 +0530

    QR reader added

--- LoanApp/app/src/com/sf/loanapp/fragments/ApplicationDetailsFragment.java
+++ LoanApp/app/src/com/sf/loanapp/fragments/ApplicationDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.sf.loanapp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,17 +9,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.sf.loanapp.activities.DecoderActivity;
 import com.sf.loanapp.R;
-import com.sf.loanapp.activities.NewAppNavActivity;
 
 public class ApplicationDetailsFragment extends Fragment {
 
+    ImageView aadhaarImageview;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try{
             getActivity().setTitle("Application Details");
+
         }
         catch(NullPointerException e){
             Log.d("AppDetailsFragment",e.getMessage());
@@ -28,11 +32,23 @@ public class ApplicationDetailsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.app_details_fragment, container, false);
+        View view = inflater.inflate(R.layout.app_details_fragment, container, false);
+        aadhaarImageview = (ImageView)view.findViewById(R.id.aadar_image);
+        aadhaarImageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplicationDetailsFragment.this.getActivity(), DecoderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+
 }
